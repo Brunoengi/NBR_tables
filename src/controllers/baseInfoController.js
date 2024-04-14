93eaa4c734c06bc5d62d25dd505c880e96aec426
Bruno <bruno.if@hotmail.com>
2024-04-14 03:59:27 -0300

    create baseInfo update method by _id

--- src/controllers/baseInfoController.js
+++ src/controllers/baseInfoController.js
@@ -20,6 +20,15 @@ class BaseInfosController {
       res.status(500).json({ message: `Error to create, ${error.message}` })
     }
   }
+  static async completeUpdate(req, res) {
+    try {
+      const id = req.params.id
+      await baseInfos.findByIdAndUpdate(id, req.body)
+      res.status(200).json( {message: "Successful baseInfo update"} )
+    }catch (error){
+      res.status(500).json( {message:error.message })
+    }
+  }
 }
 
 export default BaseInfosController
--- src/routes/baseInfos.js
+++ src/routes/baseInfos.js
@@ -5,5 +5,6 @@ const routes = express()
 
 routes.get('/baseinfos', BaseInfosController.getAll)
 routes.post('/baseinfos', BaseInfosController.create)
+routes.put('/baseinfos/:id',BaseInfosController.completeUpdate)
 
 export default routes
